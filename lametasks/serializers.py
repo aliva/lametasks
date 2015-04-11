b6296c67637ab45ad8580c63cf9da933d22d7716
Ali Vakilzade <ali@vakilzade.ir>
2015-04-11 22:59:06 +0430

    show task priority as string

--- lametasks/serializers.py
+++ lametasks/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
+from .models import List
 from .models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
+    priority = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
+        exclude = (
+            "owner",
+        )
+
+    def get_priority(self, obj):
+        return obj.get_priority_display()
